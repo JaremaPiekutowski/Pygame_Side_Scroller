2313146bea5302d555f47b3795049184693bd5c1
Jarema Piekutowski <jarema.piekutowski@gmail.com>
2022-04-28 11:50:15 +0200

    Shooting bullets added, but needs more work.

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 # SIDE SCROLLER WITH PYGAME - CODING WITH RUSS
 # https://www.youtube.com/watch?v=DHgj5jhMJKg
-# 26.04.2022 - part 3, 32:52
+# 26.04.2022 - part 4, 12:26
+# STATE OF THE GAME:
+# A player sprite is created, can run left and right as well as jump.
+# It can also shoot makeshift bullets.
+# TODO:
+#  1. Shoot bullets (IN PROGRESS)
+#  2. Grenades and projectiles
+#  3. Explosions
+#  4. Collectibles
+#  5. AI
+#  6. Loading levels
+#  7. Collision
+#  8. Screen scrolling
+#  9. Main menu and restart level
+#  10. Music/SFX and screen transitions
 
-# IMPORT PYGAME
+
+# IMPORT PYGAME AND OS
 import pygame
+import os
 
 # INITIALIZE PYGAME
 pygame.init()
@@ -30,6 +46,11 @@ GRAVITY = 0.75
 # DEFINE PLAYER ACTION VARIABLES
 moving_left = False
 moving_right = False
+shoot = False
+
+# LOAD IMAGES
+# Bullet image load (convert alpha necessary!)
+bullet_img = pygame.image.load('img/icons/bullet.png').convert_alpha()
 
 # DEFINE COLOURS
 # Background colour
@@ -66,46 +87,41 @@ class Soldier(pygame.sprite.Sprite):
         self.vel_y = 0
         # Set the jump marker
         self.jump = False
+        # Set a marker if the player is in the air. We assume he's in the air
+        # until he lands on something. This is to prevent double-jumping: if
+        # a player is in the air, he shouldn't be allowed to jump once more
+        # (see the #jump section in the Soldier.move() method)
+        self.in_air = True
         # Set a marker for where the character is facing (is the character flipped)
         self.flip = False
         # Create an animation list (it'll be a list of lists)
         self.animation_list = []
         # Set a frame index for choosing frame from the animation list (at first, it's 0)
         self.frame_index = 0
-        # Set action marker (0 - idle, 1 - running...
+        # Set action marker (0 - idle, 1 - running, 2 - jumping...
         self.action = 0
         # Set update time (for updating animation (get baseline for animation sequence)
         self.update_time = pygame.time.get_ticks()
 
-        # Load the animation lists.
-        # 1. "Idle" animation
-        # Loop through "Idle" folder and create a list of idle animation frames
-        # Create a temporary list
-        temp_list = []
-        # Range (5) because there are 5 frames in the idle animation
-        for i in range(5):
-            # Load i-th image from "Idle" directory
-            img = pygame.image.load(f'img/{self.char_type}/Idle/{i}.png')
-            # Transform the image according to the scale
-            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-            # Add the image to the list as the next frame
-            temp_list.append(img)
-        # Append the "Idle" frame list to the animation list of lists
-        self.animation_list.append(temp_list)
-
-        # 2. "Run" animation
-        # Create a new temporary list (zero the temp_list variable)
-        temp_list = []
-        # Loop through "Run" folder and create a list of run animation frames
-        for i in range(6):
-            # Load i-th image from "Run" directory
-            img = pygame.image.load(f'img/{self.char_type}/Run/{i}.png')
-            # Transform the image according to the scale
-            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-            # Add the image to the list as the next frame
-            temp_list.append(img)
-        # Append the "Run" frame list to the animation list
-        self.animation_list.append(temp_list)
+        # Load all images for the players
+        # Specify animation types
+        animation_types = ['Idle', 'Run', 'Jump']
+        # Loop through animation types and create a list of animation frames for each
+        for animation in animation_types:
+            # Create/reset a temporary list of frames
+            temp_list = []
+            # Count number of files in the folder - use "os" library
+            num_of_frames = len(os.listdir(f'img/{self.char_type}/{animation}/'))
+            # Loop every folder for all frames
+            for i in range(num_of_frames):
+                # Load i-th image from "Idle" directory
+                img = pygame.image.load(f'img/{self.char_type}/{animation}/{i}.png').convert_alpha()
+                # Transform the image according to the scale
+                img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+                # Add the image to the list as the next frame
+                temp_list.append(img)
+                # Append the "Idle" frame list to the animation list of lists
+            self.animation_list.append(temp_list)
 
         # Set current self image - get the frame from the animation_list of lists:
         # list [self.action] at index [self.frame_index]
@@ -139,9 +155,13 @@ class Soldier(pygame.sprite.Sprite):
 
         # Jump
         # Check for jumping state
-        if self.jump:
+        if self.jump and not self.in_air:
+            # If the character jumps, give him velocity on y axis -11 px
             self.vel_y = -11
+            # Reset the jump marker
             self.jump = False
+            # Reset the in_air marker
+            self.in_air = True
 
         # Apply gravity (the velocity shouldn't exceed 10)
         self.vel_y += GRAVITY
@@ -150,11 +170,13 @@ class Soldier(pygame.sprite.Sprite):
         # Change delta y accordingly (it changes if the player jumps)
         dy += self.vel_y
 
-        # Check collision with floor
+        # Check collision with floor (landing)
         if self.rect.bottom + dy > 300:
             # If there's collision, the delta y must be equal to the difference
             # between 300 (the floor level) and the bottom of the player's feet.
             dy = 300 - self.rect.bottom
+            # The player is not in the air anymore
+            self.in_air = False
 
         # Move the player rectangle (update its position)
         self.rect.x += dx
@@ -198,6 +220,34 @@ class Soldier(pygame.sprite.Sprite):
         screen.blit(pygame.transform.flip(surface=self.image, flip_x=self.flip, flip_y=False), self.rect)
 
 
+# CREATE THE BULLET CLASS (ALSO INHERITS FROM SPRITE)
+class Bullet(pygame.sprite.Sprite):
+    # Create the constructor for the Bullet class
+    # (args: x, y -> coordinates,
+    #        direction -> where the bullet goes)
+    def __init__(self, x, y, direction):
+        # Initialize Sprite
+        pygame.sprite.Sprite.__init__(self)
+        # Set speed (in this case all bullets have the same speed)
+        self.speed = 10
+        # Define image
+        self.image = bullet_img
+        # Create rectangle for bullet
+        self.rect = self.image.get_rect()
+        # Set position of the rectangle's center
+        self.rect.center = (x, y)
+        # Set direction
+        self.direction = direction
+
+    def update(self):
+        self.rect.x += (self.speed * self.direction)
+
+
+# CREATE GROUPS OF SPRITES
+# This allows to control many sprites calling one method for all
+bullet_group = pygame.sprite.Group()
+
+
 # INSTANTIATE SOLDIERS: PLAYER, ENEMY
 player = Soldier(char_type='player', x=200, y=200, scale=3, speed=5)
 enemy = Soldier(char_type='enemy', x=300, y=200, scale=3, speed=2)
@@ -217,15 +267,35 @@ while run:
     # Put the enemy on the screen
     enemy.draw()
 
+    # Update and draw groups
+    bullet_group.update()
+    bullet_group.draw(screen)
+
     # Update player action - as long as the player is alive
     if player.alive:
-        if moving_left or moving_right:
+        # Shoot bullets - instantiate Bullet next to the barrel of the gun
+        # centerx, centery -> the middle of the player
+        # player.rect.size[0] -> width of the player
+        # player direction is 1 or -1
+        if shoot:
+            bullet = Bullet(player.rect.centerx + (0.6 * player.rect.size[0] * player.direction),
+                            player.rect.centery,
+                            player.direction)
+            # Add the bullet to the bullet group
+            bullet_group.add(bullet)
+        # Jump
+        if player.in_air:
+            player.update_action(2) # 2 -> jump
+        # Move
+        elif moving_left or moving_right:
             player.update_action(1) # 1 -> run
+        # Stay idle
         else:
             player.update_action(0) # 0 -> idle
 
     # Move the player
-    player.move(moving_left=moving_left, moving_right=moving_right)
+    player.move(moving_left=moving_left,
+                moving_right=moving_right)
 
     # Call in the event handler (listen for events)
     for event in pygame.event.get():
@@ -243,6 +313,8 @@ while run:
                 moving_right = True
             if event.key == pygame.K_UP and player.alive:
                 player.jump = True
+            if event.key == pygame.K_SPACE:
+                shoot = True
             # Quitting by Esc key
             if event.key == pygame.K_ESCAPE:
                 run = False
@@ -252,6 +324,8 @@ while run:
                 moving_left = False
             if event.key == pygame.K_RIGHT:
                 moving_right = False
+            if event.key == pygame.K_SPACE:
+                shoot = False
     # Update display
     pygame.display.update()
 
